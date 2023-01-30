fc2576d83873b8d62a0459d434dde592c6c4fc9a
ChristianLopezDe <clopez88@depaul.edu>
2023-01-29 21:26:45 -0600

    change databases to postgresql

--- homework/settings.py
+++ homework/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'homework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
